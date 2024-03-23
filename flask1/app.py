ec8a98fafee342afdcce808e08d91103c72fe4a5
Alexey <slovar27@gmail.com>
2024-03-23 21:33:35 +0200

    flask routes

--- flask1/app.py
+++ flask1/app.py
@@ -8,11 +8,39 @@ def hello_world():
     return "<p>Hello, World!</p>"
 
 
+@app.route('/hello/<username>')
+def abc(username):
+    return "Hello " + username
+
+
+@app.route('/greet/<timing>/<username>')
+def greet(timing,username):
+    return "Good  " + timing + " " + username
+
+
+
+@app.route("/alexey")
+def hello_alexey():
+    print("called hello Alexey2")
+    return "<h3>Hello, Alexey2!</h3>"
+
+
+@app.route("/victor")
+def hello_victor():
+    return "<h3>Hello, Victor</h3>"
+
+
+@app.route("/user/add")
+def add_user():
+    return "User add logic"
+
+
 if __name__ == "__main__":
-    app.run('0.0.0.0', 3010)
+    app.run('0.0.0.0', 3010, debug=True)
 
 
 '''
+pip3 install flask
 python3 app.py
 
 '''

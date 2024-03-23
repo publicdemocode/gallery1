26f36d75ef9589f725fae0cb6ae3142ae7866e1b
Alexey <slovar27@gmail.com>
2024-03-23 20:54:02 +0200

    flask1 hello worls

---
+++ flask1/app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+
+if __name__ == "__main__":
+    app.run('0.0.0.0', 3010)
+
+
+'''
+python3 app.py
+
+'''

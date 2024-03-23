c1efa4986388cc1abc622704e542c49af93bec17
Alexey <slovar27@gmail.com>
2024-03-23 22:29:09 +0200

    some changes

--- flask1/app.py
+++ flask1/app.py
@@ -1,6 +1,43 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
+@app.route("/show_user/<int:user_id>")
+def show_user(user_id):
+    
+    #out = "<div style='font-size: 18'>User name: {0} <br> Live in: {1} <br> Like: {2} <br> Favirites: {3}</div>" . format(name, city, food, favorites)
+    #return out
+
+    user_data = get_user_data(user_id)
+
+    return render_template('show_user.html', user_data = user_data)
+
+
+def get_user_data(user_id):
+
+    if user_id == 1:
+        user_data = {
+            "user_id" : 1,
+            "name" : "alexey",
+            "city" : "Petah Tikva",  
+            "user_food" : "salad",
+            "user_favorites" : "Travelling, Cookking, Playing football",
+            "tel" : "058",
+            "color" : "green"
+        }
+
+    if user_id == 2:
+        user_data = {
+            "user_id" : 2,
+            "name" : "Victor",
+            "city" : "Ashkelon",  
+            "user_food" : "salad",
+            "user_favorites" : "Travelling, Cookking, Playing football",
+            "tel" : "052",
+            "color" : "green"
+        }
+
+    return user_data
 
 
 @app.route("/")
@@ -15,7 +52,7 @@ def abc(username):
 
 @app.route('/greet/<timing>/<username>')
 def greet(timing,username):
-    return "Good  " + timing + " " + username
+    return "Good  " + str(timing) + " " + str(username)
 
 
 
